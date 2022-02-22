f0081a24bbce9621b174342d4aead7cbca0814ab
czoido <mrgalleta@gmail.com>
2022-02-22 08:52:01 +0100

    remove comments

--- recipes/zlib/all/conanfile.py
+++ recipes/zlib/all/conanfile.py
@@ -129,7 +129,5 @@ class ZlibConan(ConanFile):
         self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.libs = ["zlib" if self.settings.os == "Windows" and not self.settings.os.subsystem else "z"]
 
-
-        # !!! .names has to be mocked
         self.cpp_info.names["cmake_find_package"] = "ZLIB"
         self.cpp_info.names["cmake_find_package_multi"] = "ZLIB"
