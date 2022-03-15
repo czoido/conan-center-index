4c2cfb67ecb6332187966836377b38867f557409
czoido <mrgalleta@gmail.com>
2022-03-15 08:37:56 +0100

    fix

--- recipes/fmt/all/conanfile.py
+++ recipes/fmt/all/conanfile.py
@@ -47,7 +47,7 @@ class FmtConan(ConanFile):
             tc.variables["FMT_INSTALL"] = True
             tc.variables["FMT_LIB_DIR"] = "lib"
             if self._has_with_os_api_option:
-                self._cmake.definitions["FMT_OS"] = self.options.with_os_api
+                tc.variables["FMT_OS"] = self.options.with_os_api
             tc.generate()  
 
     def layout(self):
