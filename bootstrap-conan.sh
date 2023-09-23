15a76905c5221828a725ab9f65c7cd127f6415c3
czoido <mrgalleta@gmail.com>
2023-09-23 12:10:58 +0200

    use bars

--- bootstrap-conan.sh
+++ bootstrap-conan.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
 
-# Download conan, in this case we are installing it with pip because we
-# dont have the binaries, but the idea is to download from the conan release
+# Función para mostrar la barra de progreso
+show_progress() {
+  local step_name=$1
+  local total=10
+  local current=$2
+  local percent=$((current * 100 / total))
+  local bar_length=$((current * 20 / total))
+  local bar=$(printf "%0.s=" $(seq 1 $bar_length))
+  local spaces=$(printf "%0.s " $(seq 1 $((20 - bar_length))))
+  printf "\r\033[1;34m%-30s\033[0m [%-20s] %3d%%" "$step_name" "$bar$spaces" $percent
+}
+
+# Download conan
 
 # get routes
 script_path="$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")"
@@ -9,28 +20,44 @@ script_dir="$(dirname "$script_path")"
 conan_home="$script_dir/.conan-home"
 conan_exe="$script_dir/conan" 
 mkdir -p "$conan_home"
+show_progress "Setting up directories" 1
 
 # create a virtual environment
-virtualenv "$conan_home/.env"
-source "$conan_home/.env/bin/activate"
-pip install git+https://github.com/memsharded/conan.git@feature/git_conancenter_remote
+virtualenv "$conan_home/.env" > /dev/null 2>&1
+source "$conan_home/.env/bin/activate" > /dev/null 2>&1
+show_progress "Creating environment" 2
+
+# Install Conan
+pip install git+https://github.com/memsharded/conan.git@feature/git_conancenter_remote > /dev/null 2>&1
+show_progress "Installing Conan" 3
+
+# Create symlink for conan executable
 ln -s "$conan_home/.env/bin/conan" "$conan_exe"
+show_progress "Creating symlink" 4
 
 # Create a .conanrc file and set the conan_home
 echo "conan_home=$conan_home/.conan2" > .conanrc
+show_progress "Creating .conanrc" 5
 
+# Create conanfile.txt
 echo -e "[requires]\n#Add the libraries you want to use below this line:\n\n[generators]\nCMakeToolchain\nCMakeDeps\n" > conanfile.txt
+show_progress "Creating conanfile.txt" 6
 
 # Create default profile
-
-"$conan_exe" profile detect --force
+"$conan_exe" profile detect --force > /dev/null 2>&1
+show_progress "Creating default profile" 7
 
 # Set-up git remote
-
-"$conan_exe" remote remove conancenter
-"$conan_exe" remote add c3ifork "$PWD/conan-center-index" --type=local
+"$conan_exe" remote remove conancenter > /dev/null 2>&1
+"$conan_exe" remote add c3ifork "$PWD/conan-center-index" --type=local > /dev/null 2>&1
+show_progress "Setting up git remote" 8
 
 # checks
+"$conan_exe" --version > /dev/null 2>&1
+show_progress "Checking Conan version" 9
+"$conan_exe" remote list > /dev/null 2>&1
+show_progress "Listing Conan remotes" 10
 
-"$conan_exe" --version
+# Nueva línea al final
+echo ""
 "$conan_exe" remote list
